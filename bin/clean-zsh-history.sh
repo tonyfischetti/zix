56176efa7a73749b0b06ae92bcc3d26905969ae2
Tony Fischetti <tony.fischetti@gmail.com>
2021-01-06 18:38:27 -0500

    slight modification

--- bin/clean-zsh-history.sh
+++ bin/clean-zsh-history.sh
@@ -5,6 +5,6 @@ set -v
 sleep 1
 bckup $HOME/.zsh_history
 sleep 1
-cat $HOME/.zsh_history | ack -v ':delete:' | ack -v 'ntmux delete' | ack -v 'export ZCONTEXT=delete' | ack -v '/\.Trash/' | ack -v ';cd\.{0,4}$' | ack -v '0;cd \.{1,2}$' | ack -v '0;ls$' |  ack -v '0;e..{1,4}$' | ack '^:' |perl -pe 's/(\d{4}-\d{2}-\d{2})_\d+?:\d+?:\d+/$1_ATIME/g' | sponge $HOME/.zsh_history
+cat $HOME/.zsh_history | ack -v ':delete:' | ack -v 'tmux delete' | ack -v 'export ZCONTEXT=delete' | ack -v '/\.Trash/' | ack -v ';cd\.{0,4}$' | ack -v '0;cd \.{1,2}$' | ack -v '0;ls$' |  ack -v '0;e..{1,4}$' | ack '^:' |perl -pe 's/(\d{4}-\d{2}-\d{2})_\d+?:\d+?:\d+/$1_ATIME/g' | sponge $HOME/.zsh_history
 sleep 1
 
