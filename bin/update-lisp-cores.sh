9d380ad700863146e66123c5e57210cac0390486
Tony Fischetti <tony.fischetti@gmail.com>
2021-06-02 14:00:33 -0400

    clisp core dump (but commented out sbcl for now)

--- bin/update-lisp-cores.sh
+++ bin/update-lisp-cores.sh
@@ -1,8 +1,10 @@
 #!/usr/local/bin/zsh
 
 # SBCL
-sbcl --eval "(progn (load \"~/.lisp/def-cli-args.lisp\") (save-lisp-and-die \"pluto-sbcl.core\" :save-runtime-options t :purify t))";
-mv ./pluto-sbcl.core ~/.lisp/;
+# sbcl --eval "(progn (load \"~/.lisp/def-cli-args.lisp\") (save-lisp-and-die \"pluto-sbcl.core\" :save-runtime-options t :purify t))";
+# mv ./pluto-sbcl.core ~/.lisp/;
 
 # CLISP
 # (not implemented yet)
+clisp -q -x '(progn (ql:quickload :charon) (use-package :charon) (EXT:SAVEINITMEM "pluto-clisp.mem"))'
+mv ./pluto-clisp.mem ~/.lisp/;
